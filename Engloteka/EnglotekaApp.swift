24b6a8749c13ff9061319746dd782f027c1a4d99
Konstantin <konstantinivanovich.d@gmail.com>
2023-05-26 17:24:29 +0300

    Initial Commit

---
+++ Engloteka/EnglotekaApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnglotekaApp.swift
+//  Engloteka
+//
+//  Created by Konstantin Durkin on 26.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct EnglotekaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
